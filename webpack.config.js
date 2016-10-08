79eee7ddebe87ae961117deac1f57f1ed1267c51
dnzk <danishkalesaran@gmail.com>
2016-10-09 03:18:21 +0700

    make webpack works

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: `${__dirname}/src/index.js`,
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    inline: true,
+    contentBase: `${__dirname}/dist`,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader?presets[]=es2015']
+      }
+    ]
+  }
+};
