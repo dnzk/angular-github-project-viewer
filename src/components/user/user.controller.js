4591dbba44b5e27da8977fc0445fff3354e530e7
dnzk <danishkalesaran@gmail.com>
2016-10-09 17:38:32 +0700

    hook up user with readme and style it up a bit

--- src/components/user/user.controller.js
+++ src/components/user/user.controller.js
@@ -1,6 +1,8 @@
 class UserController {
   constructor($state, UserModel) {
     this.repos = [];
+    this.$state = $state;
+    this.header = `${$state.params.user}'s projects`;
 
     UserModel.getUser($state.params.user)
       .then(result => {
@@ -12,6 +14,12 @@ class UserController {
   }
   setRepos(repos) {
     this.repos = repos;
+  }
+  goToRepo(repo) {
+    this.$state.go('project', {
+      user: this.$state.params.user,
+      project: repo
+    });
   }
 }
 
