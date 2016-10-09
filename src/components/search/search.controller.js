d5a268ae900794357238234436606d8fa1e167f2
dnzk <danishkalesaran@gmail.com>
2016-10-09 16:45:03 +0700

    tidy up search component

--- src/components/search/search.controller.js
+++ src/components/search/search.controller.js
@@ -1,22 +1,13 @@
 class SearchController {
-  constructor(UserModel, $state) {
-    this.UserModel = UserModel;
+  constructor($state) {
     this.$state = $state;
   }
   search(username) {
-    this.$state.go('user', {
-      user: username
-    });
-    // let $state = this.$state;
-    // if (username) {
-    //   this.UserModel.getRepo(username)
-    //     .then(function (result) {
-    //       $state.go('project');
-    //     })
-    //     .catch(function (error) {
-    //       console.log(error);
-    //     });
-    // }
+    if (username) {
+      this.$state.go('user', {
+        user: username
+      });
+    }
   }
 }
 
