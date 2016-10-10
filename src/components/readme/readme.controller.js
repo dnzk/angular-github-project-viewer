1a5ad9dcc52b15fa06db83e97f327331c2b04d6c
dnzk <danishkalesaran@gmail.com>
2016-10-10 09:16:14 +0700

    take care of no readme repo

--- src/components/readme/readme.controller.js
+++ src/components/readme/readme.controller.js
@@ -4,17 +4,23 @@ class ReadmeController {
   constructor($state, UserModel, $sce) {
     this.$sce = $sce;
     this.converter = new showdown.Converter();
-    console.log($state.params.project);
     this.header = `${$state.params.user} / ${$state.params.project}`;
 
     UserModel.getRepo($state.params.user, $state.params.project)
       .then((response) => {
         this.setReadmeContent(atob(response.data.content));
       })
+      .catch((error) => {
+        this.setNoContent();
+      })
   }
   setReadmeContent(mdContent) {
     let html = this.converter.makeHtml(mdContent);
     this.content = this.$sce.trustAsHtml(html);
+  }
+  setNoContent() {
+    // this.content = `No content to display`;
+    this.content = this.$sce.trustAsHtml('<h1>This repo has no readme</h1>');
   }
 }
 
